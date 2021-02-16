b8e5c758296d58dd88e758f95a4a3fbf6e57a360
yuki-katayama <katayu810@gmail.com>
2021-02-16 22:20:16 +0900

    久しぶりのコミット

--- raycast.js
+++ raycast.js
@@ -5,9 +5,14 @@ const MAP_NUM_COLS = 15;
 const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
 const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 
+const FOV_ANGLE = 60 * (Math.PI / 180);
+
+const WALL_STRIP_WIDTH = 30;
+const NUM_RAYS = WINDOW_WIDTH / WALL_STRIP_WIDTH;
+
 class Map {
     constructor() {
-        this.grid = [
+        this.grid = [ //15 * 11
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
             [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
@@ -21,14 +26,14 @@ class Map {
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
     }
-    hasWallAt(x, y) {
-        if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT) {
+    hasWallAt(x, y){
+        if(x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT)
+        {
             return true;
         }
-        console.log(WINDOW_WIDTH, WINDOW_HEIGHT, x, y)
         var mapGridIndexX = Math.floor(x / TILE_SIZE);
         var mapGridIndexY = Math.floor(y / TILE_SIZE);
-        return this.grid[mapGridIndexY][mapGridIndexX] != 0;
+        return this.grid[mapGridIndexY][mapGridIndexX] == 1;
     }
     render() {
         for (var i = 0; i < MAP_NUM_ROWS; i++) {
@@ -48,64 +53,89 @@ class Player {
     constructor() {
         this.x = WINDOW_WIDTH / 2;
         this.y = WINDOW_HEIGHT / 2;
-        this.radius = 3;
-        this.turnDirection = 0; // -1 if left, +1 if right
-        this.walkDirection = 0; // -1 if back, +1 if front
+        this.radius = 3; //描写毎の列
+        this.turnDirection = 0;
+        this.walkDirection = 0;
         this.rotationAngle = Math.PI / 2;
         this.moveSpeed = 2.0;
-        this.rotationSpeed = 2 * (Math.PI / 180);
+        this.rotationSpeed = 2 * (Math.PI / 180)
     }
-    update() {
+    update(){
         this.rotationAngle += this.turnDirection * this.rotationSpeed;
-
         var moveStep = this.walkDirection * this.moveSpeed;
-        
+        // this.x += Math.cos(this.rotationAngle) * moveStep;
+        // this.y += Math.sin(this.rotationAngle) * moveStep;
         var newPlayerX = this.x + Math.cos(this.rotationAngle) * moveStep;
         var newPlayerY = this.y + Math.sin(this.rotationAngle) * moveStep;
-
-        if (!grid.hasWallAt(newPlayerX, newPlayerY)) {
+        if (!grid.hasWallAt(newPlayerX, newPlayerY)){
             this.x = newPlayerX;
             this.y = newPlayerY;
         }
-    }
-    render() {
+    };
+    render(){
         noStroke();
         fill("red");
         circle(this.x, this.y, this.radius);
         stroke("red");
+        line(this.x, this.y, this. x + Math.cos(this.rotationAngle) * 30, this.y + Math.sin(this.rotationAngle) * 30)
+    }
+}
+
+class Ray {
+    constructor(rayAngle){
+        this.rayAngle = rayAngle;
+    }
+    render(){
+        stroke("red")
         line(
-            this.x,
-            this.y,
-            this.x + Math.cos(this.rotationAngle) * 30,
-            this.y + Math.sin(this.rotationAngle) * 30
-        );
+            player.x,
+            player.y,
+            player.x + Math.cos(this.rayAngle) * 30,
+            player.y + Math.sin(this.rayAngle) * 30
+        )
     }
 }
 
 var grid = new Map();
 var player = new Player();
+var rays = [];
 
-function keyPressed() {
-    if (keyCode == UP_ARROW) {
-        player.walkDirection = +1;
-    } else if (keyCode == DOWN_ARROW) {
-        player.walkDirection = -1;
-    } else if (keyCode == RIGHT_ARROW) {
-        player.turnDirection = +1;
-    } else if (keyCode == LEFT_ARROW) {
-        player.turnDirection = -1;
+function keyPressed(){
+    if (keyCode == UP_ARROW){
+        player.walkDirection += 1;
+    } else if(keyCode == DOWN_ARROW){
+        player.walkDirection -= 1;
+    } else if(keyCode == RIGHT_ARROW){
+        player.turnDirection += 1;
+    } else if(keyCode == LEFT_ARROW){
+        player.turnDirection -= 1;
     }
 }
 
-function keyReleased() {
-    if (keyCode == UP_ARROW) {
+function keyReleased(){
+    if (keyCode == UP_ARROW){
         player.walkDirection = 0;
-    } else if (keyCode == DOWN_ARROW) {
+    } else if(keyCode == DOWN_ARROW){
         player.walkDirection = 0;
-    } else if (keyCode == RIGHT_ARROW) {
+    } else if(keyCode == RIGHT_ARROW){
         player.turnDirection = 0;
-    } else if (keyCode == LEFT_ARROW) {
+    } else if(keyCode == LEFT_ARROW){
         player.turnDirection = 0;
+    }
+}
+
+function castAllRays() {
+    var columnId = 0;
+    var rayAngle = player.rotationAngle - (FOV_ANGLE / 2)
+
+    rays = [];
+
+    for (var i = 0; i < NUM_RAYS; i++)
+    {
+        var ray = new Ray(rayAngle)
+        rays.push(ray)
+        rayAngle +=  FOV_ANGLE / NUM_RAYS;
+        columnId++;
     }
 }
 
@@ -115,11 +145,15 @@ function setup() {
 
 function update() {
     player.update();
+    castAllRays();
+    // TODO: update all game objects before we render the next frame
 }
 
 function draw() {
     update();
-
     grid.render();
+    for (ray of rays){
+        ray.render();
+    }
     player.render();
 }
